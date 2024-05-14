f669360832a5edd591f0ae7b04ecde8467a4f5da
mustur <lorenzo.moriconi1998@gmail.com>
2024-05-14 19:28:49 +0200

    auto-commit

--- philo/srcs/monitor.c
+++ philo/srcs/monitor.c
@@ -1,32 +1,42 @@
 #include "philo.h"
 #include <unistd.h>
 
-void	kill_phil(t_table *table, int i);
+void	kill_phil(t_table *table, int i, int flag);
 
 void	check_health(void *tableu)
 {
 	int	i;
-	t_table	*table;
-	//long long startime;
+	t_table	*t;
+	int	end;
 
-	table = (t_table *)tableu;
-	//startime = table->args.start_time;
-	i = 0;
+	t = (t_table *)tableu;
 	while (1)
 	{
-		usleep(5);
-		if (!table->philos[i].eat_flag && 
-			millitime() >= table->philos[i].last_meal + table->args.tt_die)
+		end = 0;
+		i = -1;
+		while (++i < t->args.n_philos)
+		{
+			if ((t->args.n_meals != 0 && end == t->args.n_meals) 
+				||	(!t->philos[i].eat_flag 
+				&& millitime() >= t->philos[i].last_meal + t->args.tt_die))
+				break ;
+			if (t->args.n_meals != 0 && t->philos[i].meals_count >= t->args.n_meals)
+				end++;
+		}
+		if ((t->args.n_meals != 0 && end == t->args.n_philos) 
+			|| i != t->args.n_philos)
 			break ;
-		i = (i + 1) % table->args.n_philos;
 	}
-	kill_phil(table,  i);
+	kill_phil(t,  i, end != 0 && end != t->args.n_philos);
 }
 
-void	kill_phil(t_table *table, int i)
+void	kill_phil(t_table *table, int i, int killflag)
 {
 //	pthread_mutex_lock(table->philos[0].write);
 	*table->philos[0].stop = 1;
 	table->isdead = 1;
-	printf("%lli %i has died\n", millitime() - table->args.start_time, i + 1);
+	if (killflag)
+		printf("%lli %i has died\n", millitime() - table->args.start_time, i + 1);
+	else
+		printf("\n");
 }
