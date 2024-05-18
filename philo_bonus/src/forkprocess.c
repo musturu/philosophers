dbe03731171caf89d86f3eaf84d974dcd5ff4837
mustur <lorenzo.moriconi1998@gmail.com>
2024-05-18 18:15:37 +0200

    headers

--- philo_bonus/src/forkprocess.c
+++ philo_bonus/src/forkprocess.c
@@ -1,42 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   monitor.c                                          :+:      :+:    :+:   */
+/*   forkprocess.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmoricon <lmoricon@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/18 18:14:45 by lmoricon          #+#    #+#             */
-/*   Updated: 2024/05/18 18:14:46 by lmoricon         ###   ########.fr       */
+/*   Created: 2024/05/18 18:14:37 by lmoricon          #+#    #+#             */
+/*   Updated: 2024/05/18 18:15:06 by lmoricon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-#include <semaphore.h>
 #include <unistd.h>
 
-void	kill_phil(t_phil *table);
-
-void	check_health(void *phil)
+int	run_threads(t_table *table)
 {
 	int	i;
-	t_phil	*p;
-
-	p = (t_phil *)phil;
-	while (1)
-	{
-		if (( !p->eat_flag && millitime() >= p->last_meal + p->args.tt_die))
-			break ;
-		i = (i + 1) % p->args.n_philos;
-	}
-	kill_phil(p);
-}
-
-void	kill_phil(t_phil *table)
-{
-	int	i;
+	pid_t	*pid;
 
 	i = -1;
-	msg_lock("has died", table->write, *table);
+	pid = malloc(sizeof(pid_t) * table->args.n_philos);
+	if (!pid)
+		return 1;
 	while (++i < table->args.n_philos)
-		sem_post(table->stop);
+	{
+		pid[i] = fork();
+		if (!pid[i])
+		{
+			eat_sleep_repeat(table->philos + i);
+			return (1);
+		}
+	}
+	table->pid = pid;
+	return (0);
 }
