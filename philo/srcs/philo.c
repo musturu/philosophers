bbb5f23e140083a99a615e5a4bf4dab32b4b3392
Lorenzo Moriconi <lmoricon@e3r5p5.42roma.it>
2024-06-12 16:06:36 +0200

    done

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmoricon <lmoricon@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: lmoricon <lmoricon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 18:12:55 by lmoricon          #+#    #+#             */
-/*   Updated: 2024/05/18 18:12:57 by lmoricon         ###   ########.fr       */
+/*   Updated: 2024/06/12 16:05:53 by lmoricon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(int argc, char **argv)
 
 	if (!validate(argc, argv))
 		return (printf(BAD_ARGS));
+	if (ft_atoi(argv[1]) == 1)
+		return (printf("0 1 has died\n"));
 	if (!initialize(argc, argv, &table))
 		return (throw_error("Initializing", &table));
 	if (run_threads(&table))
